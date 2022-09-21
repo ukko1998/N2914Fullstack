ed5f3d5d58c6508b61164f45517c24837758c732
ukko1998 <67655760+ukko1998@users.noreply.github.com>
2022-09-21 18:34:01 +0300

    Add files via upload

---
+++ H4/H4T1/H4T1.js
@@ -0,0 +1,48 @@
+async function getHouses() {
+  let url = 'talotiedot.json';
+  try {
+    let res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function renderHouses() {
+  let houses = await getHouses();
+  console.log(houses);
+
+  let housediv = document.getElementById('houses');
+
+  houses.forEach((house) => {
+    housecontainer = document.createElement('div');
+    housecontainer.className = 'houseContainer';
+
+    let image = document.createElement('img');
+    image.src = house.image;
+    image.className = 'houseImage';
+
+    let header = document.createElement('p');
+    header.className = 'header';
+    header.innerHTML = house.address;
+
+    /*
+           Ohjelmoi tähän toiminnallisuus, jossa luodaan p-elementit
+           myös talon koon, tekstikuvauksen ja hinnan näyttämiselle
+           Hinnan voit muotoilla tuhaterottimia käyttävään suomalaiseen
+           muotoon esim:
+        let numberstr = new Intl.NumberFormat('fi-FI').format(house.price);
+      */
+
+    housecontainer.appendChild(image);
+    housecontainer.appendChild(header);
+
+    /* Ohjelmoi tähän toiminnallisuus, jossa liität edellä
+                  luomasi p-elementit myös housecontaineriin
+              */
+
+    housediv.appendChild(housecontainer);
+  });
+}
+
+renderHouses(); //ohjelman aloittaminen

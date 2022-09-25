db22dd173482400741619613a30dc69ba0063ad6
ukko1998 <67655760+ukko1998@users.noreply.github.com>
2022-09-25 18:40:50 +0300

    Update H5T1.js

--- H5/H5T1.js
+++ H5/H5T1.js
@@ -1,8 +1,9 @@
+// H5T1.js
 const nimi = 'Tommi Martikainen';
 
 console.log(nimi);
 
-//lähettämällä kutsumalla nodella tätä koodia H5T1.js, eli:
+//kutsumalla nodella tätä JavaScript tiedostoa H5T1.js, eli:
 //node H5T1.js
 //tulostuu nimi muuttujan sisältö:
 //Tommi Martikainen

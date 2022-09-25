1390e67ccd909b8ba1bbed28c6b829809ac7698d
ukko1998 <67655760+ukko1998@users.noreply.github.com>
2022-09-25 18:30:42 +0300

    Add files via upload

---
+++ H5/H5T1.js
@@ -0,0 +1,8 @@
+const nimi = 'Tommi Martikainen';
+
+console.log(nimi);
+
+//lähettämällä kutsumalla nodella tätä koodia H5T1.js, eli:
+//node H5T1.js
+//tulostuu nimi muuttujan sisältö:
+//Tommi Martikainen

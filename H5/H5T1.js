3d87991f26122d396f74ac4eb7c511a7a61ab6f3
ukko1998 <67655760+ukko1998@users.noreply.github.com>
2022-09-25 18:49:32 +0300

    Update H5T1.js

--- H5/H5T1.js
+++ H5/H5T1.js
@@ -1,9 +1,14 @@
 // H5T1.js
-const nimi = 'Tommi Martikainen';
 
+const nimi = 'Tommi Martikainen';
 console.log(nimi);
 
-//kutsumalla nodella tätä JavaScript tiedostoa H5T1.js, eli:
-//node H5T1.js
-//tulostuu konsoliin/terminaaliin nimi muuttujan sisältö:
-//Tommi Martikainen
+/*
+  kutsumalla nodella tätä JavaScript tiedostoa H5T1.js, eli:
+
+node H5T1.js
+
+  tulostuu konsoliin/terminaaliin nimi muuttujan sisältö:
+  
+Tommi Martikainen
+*/
